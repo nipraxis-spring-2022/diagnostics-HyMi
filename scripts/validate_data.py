060486902e21a1766636eaf88e7643d79f6c8aad
Hyesue Jang <hyesuej@gmail.com>
2022-05-13 15:31:36 -0700

    fixed validate_data

--- scripts/validate_data.py
+++ scripts/validate_data.py
@@ -24,8 +24,13 @@ def file_hash(filename):
         SHA1 hexadecimal hash string for contents of `filename`.
     """
     # Open the file, read contents as bytes.
+    fobj = open(filename, 'rb')
+    file_bytes = fobj.read()
+    fobj.close()
+
     # Calculate, return SHA1 has on the bytes from the file.
-    raise NotImplementedError('This is just a template -- you are expected to code this.')
+    hash_value = hashlib.sha1(file_bytes).hexdigest()
+    return hash_value
 
 
 def validate_data(data_directory):
@@ -50,11 +55,24 @@ def validate_data(data_directory):
         ``hash_list.txt`` file.
     """
     # Read lines from ``hash_list.txt`` file.
+    import os.path as op
+    import numpy as np
+
+    #data_directory = '/Users/hyesue/Research/nipraxis/week5/collab/diagnostics-HyMi/data/group-01'
+    fname = op.join(data_directory, 'hash_list.txt')
+    hash_list = np.loadtxt(fname, dtype='str')
     # Split into SHA1 hash and filename
+
     # Calculate actual hash for given filename.
-    # If hash for filename is not the same as the one in the file, raise
-    # ValueError
-    raise NotImplementedError('This is just a template -- you are expected to code this.')
+    for i in range(0, np.shape(hash_list)[0]):
+        fname = data_directory + '/../' + hash_list[i,1]
+
+        this_file_hash = file_hash(fname)
+
+        # If hash for filename is not the same as the one in the file, raise
+        # ValueError
+        if this_file_hash != hash_list[i,0]:
+            raise NotImplementedError('Not valid.')
 
 
 def main():
@@ -67,7 +85,6 @@ def main():
     data_directory = sys.argv[1]
     # Call function to validate data in data directory
     validate_data(data_directory)
-
 
 if __name__ == '__main__':
     # Python is running this file as a script, not importing it.
